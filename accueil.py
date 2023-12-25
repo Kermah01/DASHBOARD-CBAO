5f4006397e9eaa3ba79bc6fb9e87c2731455308b
Kermah01 <126328483+Kermah01@users.noreply.github.com>
2023-12-25 13:31:03 +0000

    Update accueil.py

--- accueil.py
+++ accueil.py
@@ -1,9 +1,11 @@
+
+#Importation des librairies
 import streamlit as st
 from CBAO_Dash import dashboard_users
 st.set_page_config(layout="wide")
 
 
-
+#Chargement de l'image en arrière-plan
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
@@ -23,6 +25,8 @@ display: flex;
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 st.markdown('<div style="text-align:center;width:100%;"><h1 style="color:black;background-color:#f7a900;border:#fc1c24;border-style:solid;border-radius:5px;">TABLEAU DE BORD INTERACTIF DE LA BOITE A IDEES DIGITALE</h1></div>', unsafe_allow_html=True)
+
+#Espacement
 st.write("\n")
 st.write("\n")
 st.write("\n")
@@ -30,6 +34,7 @@ st.write("\n")
 st.write("\n")
 st.write("\n")
 st.write("\n")
+
 # Dictionnaire contenant les informations d'identification des utilisateurs autorisés
 users_credentials = {
     'cbao-qualité': {'password': '2023', 'Zone': 'cbao-qualité'},
